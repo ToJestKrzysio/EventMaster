ed42bec16eb344f156e36dd63ab083b38d33123c
ToJestKrzysio <krzysztof.plonka64@gmail.com>
2021-12-02 18:18:50 +0100

    fix for tests

--- backend/event/tests/conftest.py
+++ backend/event/tests/conftest.py
@@ -10,11 +10,18 @@ from event.models import Event, Registration
 
 @pytest.fixture
 def event_db(db):
-    user = get_user_model().create(
+    user = get_user_model().objects.create(
         username="testuser",
         password="NotVeryStrongPassword",
         email="testmail@testmail.com"
     )
+    admin = get_user_model().objects.create(
+        username="superuser",
+        password="NotVeryStrongPassword",
+        email="adminmail@testmail.com"
+    )
+    admin.superuser = True
+    admin.save()
     event = Event.objects.create(
         title="Test Event",
         description="This is test Event",
@@ -24,14 +31,15 @@ def event_db(db):
         price=666,
         max_occupancy=13,
         location="D2 404",
+        creator=admin,
     )
     Registration.objects.create(
         event=event, user=user,
-        payment_completed=False,
+        payment_completed=True,
         payment_deadline=timezone.now() + timedelta(days=2),
     )
 
 
 @pytest.fixture
-def event_list_view_response(client, db):
+def event_list_view_response(client, event_db):
     return client.get(reverse("event:event_list"))
